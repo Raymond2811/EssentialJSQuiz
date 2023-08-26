1d45d60617b2965a71f5940ec37476a359f27ce7
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-08-25 20:21:31 -0700

    added explosion

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const timeEl = document.querySelector('.time');
 const mainEl = document.getElementById('bomb');
 
-let secondsLeft = 81
+let secondsLeft = 3
 
 function setTime() {
     const timerInterval = setInterval(function(){
@@ -10,16 +10,27 @@ function setTime() {
 
         if(secondsLeft === 0) {
             clearInterval(timerInterval);
-            sendMessage();
+            showExplosion();
         }
     },1000);
 }
 
-function sendMessage() {
-    timeEl.textContent = ' ';
+function showExplosion() {
+    removeAllElements();
+
+    timeEl.textContent = '';
     const imgEl = document.createElement('img');
-    imgEl.setAttribute('src', 'assets/images/explosion.jpeg')
-    mainEl.appendChild(imgEl)
+    imgEl.setAttribute('src', 'assets/images/explosion.jpeg');
+    imgEl.style.top = '0';
+    imgEl.style.left = '0';
+    imgEl.style.width = '100vw';
+    imgEl.style.height = '100vh';
+    imgEl.style.zIndex = '9999'; 
+    document.body.appendChild(imgEl)
+}
+
+function removeAllElements() {
+    document.body.innerHTML = '';
 }
 
 setTime();
