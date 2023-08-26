09cd5bcf6ab6ef67af526df75a5c5217a660eb0d
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-08-25 17:51:27 -0700

    added countdown timer and image

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const timeEl = document.querySelector('.time');
-const mainEl = document.getElementById('main');
+const mainEl = document.getElementById('bomb');
 
-const secondsLeft = 80
+let secondsLeft = 81
 
+function setTime() {
+    const timerInterval = setInterval(function(){
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    },1000);
+}
+
+function sendMessage() {
+    timeEl.textContent = ' ';
+    const imgEl = document.createElement('img');
+    imgEl.setAttribute('src', 'assets/images/explosion.jpeg')
+    mainEl.appendChild(imgEl)
+}
+
+setTime();
